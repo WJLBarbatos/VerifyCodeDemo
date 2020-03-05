7e0466bc21bdf0cb1f44a149af5ca13768df63b7
wujl100 <wujl100@chinaunicom.cn>
2020-03-05 15:06:44 +0800

    实现功能

--- VerifyCodeDemo_h5/src/components/HelloWorld.vue
+++ VerifyCodeDemo_h5/src/components/HelloWorld.vue
@@ -1,113 +1,150 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+    <div id="hello" v-cloak>
+        <div>
+            <input
+                id="loginphone"
+                type="tel"
+                class="login-phone"
+                placeholder="请输入手机号"
+                maxlength="11"
+                v-model="inputphone"
+            />
+        </div>
+
+        <div class="valicode-con">
+            <input
+                id="logincode"
+                type="tel"
+                class="login-valicode"
+                placeholder="请输入验证码"
+                maxlength="6"
+                v-model="inputvalicode"
+            />
+            <button
+                id="getcode"
+                type="button"
+                v-bind:disabled="!inputNum"
+                v-on:click="reqValicode()"
+                v-bind:class="{ 'login-uninputphone': !inputNum, 'login-inputphone': inputNum }"
+            >{{getvalicode}}</button>
+        </div>
+    </div>
 </template>
 
 <script>
+const TIME_COUNT = 60; //验证码倒计时
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    methods: {
+        startValicodeTimer: function() {
+            if (!this.timer) {
+                this.count_Timer = TIME_COUNT;
+                this.timer = setInterval(() => {
+                    if (this.count_Timer > 0) {
+                        this.inputNum = false;
+                        this.getvalicode = this.count_Timer + "s后重新获取";
+                        this.count_Timer--;
+                    } else {
+                        this.inputNum = true;
+                        this.getvalicode = "获取验证码";
+                        clearInterval(this.timer);
+                        this.timer = null;
+                    }
+                }, 1000);
+            }
+        },
+        //获取验证码
+        reqValicode: function() {
+            var userphone = this.inputphone;
+
+            if (
+                isNaN(userphone) ||
+                userphone.indexOf("1") != 0 ||
+                userphone.length != 11
+            ) {
+                this.$layer.open({
+                    content: "请输入有效的手机号",
+                    skin: "msg",
+                    time: 2
+                });
+                return false;
+            }
+
+            this.startValicodeTimer();
+
+            //TODO 调用服务端获取验证码接口
+        }
+    },
+    data: function() {
+        return {
+            count_Timer: "",
+            timer: null,
+            //输入框监听
+            inputphone: "",
+            inputvalicode: "",
+            getvalicode: "获取验证码",
+            inputNum: false
+        };
+    },
+    watch: {
+        inputphone: function() {
+            var phone = this.inputphone;
+            if (phone.length == 11) {
+                this.inputNum = true;
+            } else {
+                this.inputNum = false;
+            }
+        }
     }
-  }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.login-phone {
+    width: 8rem;
+    height: 1.2rem;
+    border: 1px solid #ebebeb;
+    border-radius: 0.1rem;
+    font-size: 0.35rem;
+    text-indent: 0.3rem;
+    margin-top: 1rem;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.valicode-con {
+    margin-top: 0.4rem;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.login-valicode {
+    height: 1.2rem;
+    border: 1px solid #ebebeb;
+    border-radius: 0.1rem;
+    font-size: 0.35rem;
+    width: 4.7rem;
+    text-indent: 0.3rem;
 }
-a {
-  color: #42b983;
+
+.login-uninputphone {
+    display: inline-block;
+    height: 1.2rem;
+    line-height: 1.2rem;
+    margin-left: 0.2rem;
+    border: 1px solid #ebebeb;
+    background: #ffffff;
+    border-radius: 0.1rem;
+    font-size: 0.35rem;
+    width: 3rem;
+    color: #bbbbbb;
+}
+
+.login-inputphone {
+    display: inline-block;
+    height: 1.2rem;
+    line-height: 1.2rem;
+    margin-left: 0.2rem;
+    border: 1px solid #ebebeb;
+    background: #ffffff;
+    border-radius: 0.1rem;
+    font-size: 0.35rem;
+    width: 3rem;
+    color: #d8aa6c;
 }
 </style>
--- VerifyCodeDemo_h5/src/main.js
+++ VerifyCodeDemo_h5/src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'lib-flexible'
+import layer from 'vue2-layer-mobile'
 
 Vue.config.productionTip = false
+Vue.use(layer)
 
 /* eslint-disable no-new */
 new Vue({
